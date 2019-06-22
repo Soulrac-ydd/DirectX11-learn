24833b65d9a04936f790dc0df0a906877a8546b0
庭豪 胡 <652157227@qq.com>
2019-06-22 22:39:49 +0800

    Draw A Rectangle && Add Comment

--- Dx11 Learn/Draw A Triangle/main.cpp
+++ Dx11 Learn/Draw A Triangle/main.cpp
@@ -1,4 +1,4 @@
-
+﻿
 //Include and link appropriate libraries and headers//
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "d3dx11.lib")
@@ -30,8 +30,8 @@ LPCTSTR WndClassName = L"firstwindow";
 HWND hwnd = NULL;
 HRESULT hr;
 
-const int Width = 300;
-const int Height = 300;
+const int Width = 400;
+const int Height = 400;
 
 //Function Prototypes//
 bool InitializeDirect3d11App(HINSTANCE hInstance);
@@ -55,9 +55,42 @@ struct Vertex	//Overloaded Vertex Structure
 	XMFLOAT3 pos;
 };
 
+//输入数据类型的数组
 D3D11_INPUT_ELEMENT_DESC layout[] =
 {
 	{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+	/*
+	D3D11_INPUT_ELEMENT_DESC结构成员说明：
+
+		typedef struct D3D11_INPUT_ELEMENT_DESC
+		{
+			LPCSTR SemanticName;
+			UINT SemanticIndex;
+			DXGI_FORMAT Format;
+			UINT InputSlot;
+			UINT AlignedByteOffset;
+			D3D11_INPUT_CLASSIFICATION InputSlotClass;
+			UINT InstanceDataStepRate;
+		}D3D11_INPUT_ELEMENT_DESC;
+
+		SemanticName：这只是一个与元素关联的字符串。此字符串将用于将顶点结构中的元素映射到顶点着色器中的元素。（即HLSL中的语义）
+
+		SemanticIndex： 这基本上只是用作SemanticName之后的数字。例如，如果我们在顶点结构中有2个纹理元素，而不是创建2个不同的纹理语义名称，我们可以使用2个不同的索引。
+						如果顶点着色器代码中的语义名称后面没有索引，则默认为索引0.例如，在我们的着色器代码中，我们的语义名称为“POSITION”，实际上与“POSITION0”相同。
+
+		Format： 对应输入结构中元素的类型格式，必须是DXGI_FORMAT枚举类型的成员。例如输入结构中元素是描述位置的3D矢量，便可使用DXGI_FORMAT_R32G32B32_FLOAT。
+
+		InputSlot： Direct3D允许使用16个不同的元素槽（0-15），可以在其中放置​​顶点数据。如果我们的顶点结构具有位置和颜色，
+					我们可以将两个元素放在同一个输入槽中，或者我们可以将位置数据放在第一个槽中，将颜色数据放在第二个槽中。
+
+		AlignedByteOffset： 这是描述的元素的字节偏移量。在单个输入槽中，如果我们有位置和颜色，偏移量可以是0，因为它从顶点结构的开头开始，
+							颜色需要是前面的顶点位置的大小，即12个字节（例如顶点位置的格式为DXGI_FORMAT_R32G32B32_FLOAT，
+							位置中每个组件（RGB分量）占32位，总共占96位。一个字节有8位，所以96/8 == 12）。
+
+		InputSlotClass： 标识单个输入槽的输入数据类型。
+
+		InstanceDataStepRate： 仅限于InputSlotClass值为D3D11_INPUT_PER_INSTANCE_DATA时使用，InputSlotClass值为D3D11_INPUT_PER_VERTEX_DATA时将该值指定为0。
+	*/
 };
 UINT numElements = ARRAYSIZE(layout);
 ///////////////**************new**************////////////////////
@@ -221,55 +254,133 @@ void CleanUp()
 bool InitScene()
 {
 	//Compile Shaders from shader file
-	hr = D3DX11CompileFromFile(L"Effects.fx", 0, 0, "VS", "vs_4_0", 0, 0, 0, &VS_Buffer, 0, 0);
-	hr = D3DX11CompileFromFile(L"Effects.fx", 0, 0, "PS", "ps_4_0", 0, 0, 0, &PS_Buffer, 0, 0);
+	hr = D3DX11CompileFromFile(L"Effects.fx", 0, 0, "VS", "vs_5_0", 0, 0, 0, &VS_Buffer, 0, 0);
+	hr = D3DX11CompileFromFile(L"Effects.fx", 0, 0, "PS", "ps_5_0", 0, 0, 0, &PS_Buffer, 0, 0);
+	/*
+	D3DX11CompileFromFile函数说明
+
+		#ifdef UNICODE
+		#define D3DX11CompileFromFile D3DX11CompileFromFileW
+
+		HRESULT WINAPI D3DX11CompileFromFileW(
+			LPCWSTR pSrcFile,
+			CONST D3D10_SHADER_MACRO* pDefines,
+			LPD3D10INCLUDE pInclude,
+			LPCSTR pFunctionName,
+			LPCSTR pProfile,
+			UINT Flags1,
+			UINT Flags2,
+			ID3DX11ThreadPump* pPump,
+			ID3D10Blob** ppShader,
+			ID3D10Blob** ppErrorMsgs,
+			HRESULT* pHResult
+		);
+
+		pSrcFile [in]： 包含着色器代码的文件的名称。如果编译器设置需要Unicode，则数据类型LPCTSTR将解析为LPCWSTR。
+
+		pDefines [in]： 指向宏定义数组的指针。
+
+		pInclude [in]： 这是一个指向include接口的指针。如果着色器在文件中包含#include，则将此值设置为NULL将导致编译错误。
+
+		pFunctionName [in]： 这是该文件名中着色器函数的名称。
+
+		pProfile [in]： 要使用的着色器版本。 Direct3D 11支持着色器版本5.0。
+
+		Flags1 [in]： 着色器编译标志，指定编译器如何编译HLSL代码。
+
+		Flags2 [in]： 效果（Effect）编译标志，指示编译器如何编译效果文件或运行时如何处理效果文件。
+
+		pPump [in]： 与多线程有关，使用NULL指定此函数在完成之前不应返回。
+
+		ppShader [out]： 这是返回的着色器。它不是实际的着色器，而更像是包含着色器的缓冲区和有关着色器的信息。然后使用此缓冲区来创建实际的着色器。
+
+		ppErrorMsgs [out]： 返回编译期间发生的错误和警告的列表。这些错误和警告与调试器的调试输出相同。
+
+		pHResult [out]： 指向返回值的指针，如果在向pPump参数提供NULL时向pHResult参数提供非NULL值，则D3DX11CompileFromFile返回E_INVALIDARG。
+						 如果pPump不为NULL，则pHResult必须是有效的内存位置（非NULL），直到异步执行完成。
+						 总的来说，如果pPump参数为NULL，则pHResult参数也必须为NULL。
+	*/
 
 	//Create the Shader Objects
 	hr = d3d11Device->CreateVertexShader(VS_Buffer->GetBufferPointer(), VS_Buffer->GetBufferSize(), NULL, &VS);
 	hr = d3d11Device->CreatePixelShader(PS_Buffer->GetBufferPointer(), PS_Buffer->GetBufferSize(), NULL, &PS);
+	/*
+	Create(Vertex/Pixel)Shader函数说明
+
+		virtual HRESULT STDMETHODCALLTYPE Create(Vertex/Pixel)Shader(
+			__in  const void *pShaderBytecode,
+			__in  SIZE_T BytecodeLength,
+			__in_opt  ID3D11ClassLinkage *pClassLinkage,
+			__out_opt  ID3D11(Vertex/Pixel)Shader **pp(Vertex/Pixel)Shader
+		) = 0;
+
+		pShaderBytecode [in]： 这是指向着色器缓冲区开始的指针。
+
+		BytecodeLength [in]： 这是缓冲区的大小。
+
+		pClassLinkage [in]： 指向类链接对象的指针。类链接对象是多个着色器可以共享的变量和类型的命名空间。
+							 （具体参考：https://docs.microsoft.com/zh-cn/windows/desktop/direct3dhlsl/storing-variables-and-types-for-shaders-to-share）。
+
+		pp(Vertex/Pixel)Shader [out]： 这是返回的（顶点/像素）着色器。
+	*/
 
 	//Set Vertex and Pixel Shaders
 	d3d11DevCon->VSSetShader(VS, 0, 0);
 	d3d11DevCon->PSSetShader(PS, 0, 0);
 
 	//Create the vertex buffer
+	//现在我们需要创建顶点缓冲区。
+	//我们首先使用Vertex结构制作顶点数组。
+	//在我们有一个顶点数组后，我们将通过填写D3D11_BUFFER_DESC结构来描述我们的顶点缓冲区，并通过调用ZeroMemory()确保它是空的。
+	//有了缓冲区的描述后，接着需要在缓冲区中填入D3D11_SUBRESOURCE_DATA结构和我们想要的数据。
+	//最后使用刚创建的缓冲区描述和缓冲区数据来创建缓冲区。
 	Vertex v[] =
 	{
-		Vertex(0.0f, 0.5f, 0.5f),
-		Vertex(0.5f, -0.5f, 0.5f),
+		//绘制正方形顶点顺序一般是Z字形排列。
+		Vertex(-0.5f, 0.5f, 0.5f),
+		Vertex(0.5f, 0.5f, 0.5f),
 		Vertex(-0.5f, -0.5f, 0.5f),
+		Vertex(0.5f, -0.5f, 0.5f),
 	};
 
 	D3D11_BUFFER_DESC vertexBufferDesc;
 	ZeroMemory(&vertexBufferDesc, sizeof(vertexBufferDesc));
 
 	vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
-	vertexBufferDesc.ByteWidth = sizeof(Vertex) * 3;
+	vertexBufferDesc.ByteWidth = sizeof(Vertex) * 4;
 	vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
 	vertexBufferDesc.CPUAccessFlags = 0;
 	vertexBufferDesc.MiscFlags = 0;
 
 	D3D11_SUBRESOURCE_DATA vertexBufferData;
-
 	ZeroMemory(&vertexBufferData, sizeof(vertexBufferData));
 	vertexBufferData.pSysMem = v;
+
 	hr = d3d11Device->CreateBuffer(&vertexBufferDesc, &vertexBufferData, &triangleVertBuffer);
 
 	//Set the vertex buffer
+	//有了一个顶点缓冲区之后，接着需要将它绑定到IA。
 	UINT stride = sizeof(Vertex);
 	UINT offset = 0;
 	d3d11DevCon->IASetVertexBuffers(0, 1, &triangleVertBuffer, &stride, &offset);
 
 	//Create the Input Layout
+	//创建一个输入布局对象来描述IA阶段的输入缓冲区数据
 	d3d11Device->CreateInputLayout(layout, numElements, VS_Buffer->GetBufferPointer(), VS_Buffer->GetBufferSize(), &vertLayout);
 
 	//Set the Input Layout
+	//创建了输入布局之后，接下来要做的是将它作为活动输入布局绑定到IA。
 	d3d11DevCon->IASetInputLayout(vertLayout);
 
 	//Set Primitive Topology
-	d3d11DevCon->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	//接着告诉IA我们发送的图元（primitive）类型是什么。（D3D11_PRIMITIVE_TOPOLOGY枚举类型）
+	d3d11DevCon->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
 
 	//Create the Viewport
+	//然后创建并设置视口（Viewport）。
+	//视口将告诉渲染管线的RS（Rasterizer Stage）阶段要绘制什么。我们可以使用D3D11_VIEWPORT结构创建一个视口。
+	//视口是一个以像素为单位的正方形，光栅化器使用该正方形来查找在窗口的客户区域（Win32）上显示几何体的位置。
+	//因此,将框的左上角设置为0,0，将框的右下角设置为Width，Height，以像素为单位。
 	D3D11_VIEWPORT viewport;
 	ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
 
@@ -279,6 +390,9 @@ bool InitScene()
 	viewport.Height = Height;
 
 	//Set the Viewport
+	//在创建了视口之后，接着需要将它绑定到渲染管线的RS阶段。
+	//第一个参数是要绑定的视口数，第二个参数是指向视口数组的指针。
+	//这代表可以有多个“窗口”，例如玩家1一个，玩家2一个。
 	d3d11DevCon->RSSetViewports(1, &viewport);
 
 	return true;
@@ -298,7 +412,8 @@ void DrawScene()
 	d3d11DevCon->ClearRenderTargetView(renderTargetView, bgColor);
 
 	//Draw the triangle
-	d3d11DevCon->Draw(3, 0);
+	//第一个参数是要绘制的顶点数。第二个参数是要绘制的第一个顶点的索引，通常是顶点缓冲区中的偏移量。
+	d3d11DevCon->Draw(4, 0);
 
 	//Present the backbuffer to the screen
 	SwapChain->Present(0, 0);
